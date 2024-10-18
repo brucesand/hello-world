4c4e7b2ac6dc5e884357041aed502aed631ef059
Bruce Sandahl <brucesandahl@gmail.com>
2024-10-18 16:42:11 -0700

    Added name

--- src/edu/pcc/cis/hellowlrld/HelloWorld.java
+++ src/edu/pcc/cis/hellowlrld/HelloWorld.java
@@ -1,5 +1,9 @@
 package edu.pcc.cis.hellowlrld;
 
+/**
+ * Author: Bruce Sandahl
+ */
+
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
